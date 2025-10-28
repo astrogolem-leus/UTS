20d226f66593a38fb71ed43b71390128bc33acb9
astrogolem-leus <cadanganseriesikalus1kali@gmail.com>
2025-10-28 15:40:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if uploaded_file is not None:
 
             # --- Preprocessing gambar ---
             img_resized = img.resize((target_width, target_height))
-            img_array = image.img_to_array(img_resized)
+            img_array = tf.keras.preprocessing.image.img_to_array(img_resized)
             img_array = np.expand_dims(img_array, axis=0)
             img_array = img_array / 255.0
 
